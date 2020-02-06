6e28f8160f371ebd58180cb36c92ca727697c78d
Joseph Gattuso <31634907+JosephGattuso1997@users.noreply.github.com>
2020-02-05 21:13:25 -0500

    deploy

--- Algo/settings.py
+++ Algo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -123,3 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+
+django_heroku.settings(locals())
